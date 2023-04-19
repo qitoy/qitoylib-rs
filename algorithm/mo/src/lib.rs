a734eb5ab53daa2481813c98de3412d798cdc306
qitoy <itmkan@icloud.com>
2023-04-19 12:51:16 +0900

    update

--- algorithm/mo/src/lib.rs
+++ algorithm/mo/src/lib.rs
@@ -2,24 +2,23 @@
 
 use std::convert::TryInto;
 
-/// 実装が必要な4つの関数は、範囲を伸ばし縮みさせたときに呼ばれ、範囲からpush/popされたindexを用いて`val`を変更します。
 pub trait Mo {
-    type T: Default + Copy;
+    type T;
 
-    /// 範囲の左側を伸ばしたときに呼ばれます。
-    fn push_left(&mut self, val: &mut Self::T, idx: usize);
+    fn push_left(&self, val: &mut Self::T, idx: usize);
 
-    /// 範囲の左側を縮めたときに呼ばれます。
-    fn pop_left(&mut self, val: &mut Self::T, idx: usize);
+    fn pop_left(&self, val: &mut Self::T, idx: usize);
 
-    /// 範囲の右側を伸ばしたときに呼ばれます。
-    fn push_right(&mut self, val: &mut Self::T, idx: usize);
+    fn push_right(&self, val: &mut Self::T, idx: usize);
 
-    /// 範囲の右側を縮めたときに呼ばれます。
-    fn pop_right(&mut self, val: &mut Self::T, idx: usize);
+    fn pop_right(&self, val: &mut Self::T, idx: usize);
+    
+    /// * `val` - クエリに対応する値
+    /// * `idx` - クエリの何番目か
+    fn apply(&self, val: &Self::T, idx: usize);
 
-    /// 複数の範囲クエリに対して、それに対応する値を格納したベクタを返します。
-    fn solve(&mut self, query: &[(usize, usize)]) -> Vec<Self::T> {
+    /// Mo's algorithmをします。
+    fn solve(&self, query: &[(usize, usize)], init: Self::T) {
         let k = query.iter().map(|x| x.1).max().unwrap();
         let k: usize = (k.ilog2()+1).try_into().unwrap();
         let mut query: Vec<_> = query.into_iter().zip(0..).collect();
@@ -27,17 +26,15 @@ pub trait Mo {
             let (l, r) = *x.0;
             hilbert_order(k, l, r)
         });
-        let mut ret = vec![Self::T::default(); query.len()];
-        let mut val = Self::T::default();
+        let mut val = init;
         let (mut l, mut r) = (0, 0);
-        for (&(nl, nr), i) in query.into_iter() {
+        for (&(nl, nr), idx) in query.into_iter() {
             while r < nr { self.push_right(&mut val, r); r += 1; }
             while nl < l { l -= 1; self.push_left(&mut val, l); }
             while nr < r { r -= 1; self.pop_right(&mut val, r); }
             while l < nl { self.pop_left(&mut val, l); l += 1; }
-            ret[i] = val;
+            self.apply(&val, idx);
         }
-        ret
     }
 }
 
