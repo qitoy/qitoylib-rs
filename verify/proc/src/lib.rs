00b1b742744f76157911311f5cfc4b6fdf619e69
qitoy <itmkan@icloud.com>
2023-09-11 12:19:18 +0900

    disable nightly

--- verify/proc/src/lib.rs
+++ verify/proc/src/lib.rs
@@ -1,18 +1,9 @@
-#![feature(proc_macro_span)]
-
 use proc_macro::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, ItemImpl, LitStr};
 
 #[proc_macro_attribute]
 pub fn verify(attr: TokenStream, item: TokenStream) -> TokenStream {
-    let path = proc_macro::Span::call_site().source_file().path();
-    let fnname: proc_macro2::TokenStream = path
-        .file_stem()
-        .and_then(|v| v.to_str())
-        .unwrap_or("test")
-        .parse()
-        .unwrap();
     let attr = parse_macro_input!(attr as LitStr);
     let path = format!("./testcases/{}/*", attr.value());
     let imp = parse_macro_input!(item as ItemImpl);
@@ -26,7 +17,7 @@ pub fn verify(attr: TokenStream, item: TokenStream) -> TokenStream {
         #imp
         use std::path::PathBuf;
         #[rstest::rstest]
-        fn #fnname(#[files(#path)] path: PathBuf) -> anyhow::Result<()> {
+        fn test(#[files(#path)] path: PathBuf) -> anyhow::Result<()> {
             #ident::verify(path)
         }
     }.into()
