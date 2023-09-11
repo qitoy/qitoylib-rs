b9a787ea7affe9568220765be42eafe40fd48685
qitoy <itmkan@icloud.com>
2023-09-11 12:13:08 +0900

    add test (aoj 1595)

---
+++ verify/tests/rerooting_dp.rs
@@ -0,0 +1,53 @@
+use ac_library::Monoid;
+use verify::{verify, Verify};
+
+struct M;
+
+impl Monoid for M {
+    type S = (usize, usize);
+    fn identity() -> Self::S {
+        (0, 0)
+    }
+    fn binary_operation(a: &Self::S, b: &Self::S) -> Self::S {
+        (a.0 + b.0, a.1.max(b.1))
+    }
+}
+
+struct V;
+
+#[verify("aoj/1595")]
+impl Verify for V {
+    fn solve(input: &str, stdout: &mut String) {
+        use proconio::{input, marker::Usize1, source::once::OnceSource};
+        use qitoy_rerooting_dp::RerootingDP;
+
+        let mut source = OnceSource::from(input);
+
+        input! {
+            from &mut source,
+            n: usize,
+        }
+
+        let mut rdp = RerootingDP::<M>::new(n);
+
+        for _ in 1..n {
+            input! {
+                from &mut source,
+                u: Usize1, v: Usize1,
+            }
+            rdp.add_edge(u, v);
+        }
+
+        // avoid stack overflow
+        let v = std::thread::Builder::new()
+            .stack_size(n * 2048)
+            .spawn(move || rdp.build(&|v, _| (v.0 + 1, v.1 + 1), &|v, _| v))
+            .unwrap()
+            .join()
+            .unwrap();
+
+        for v in v {
+            stdout.push_str(&format!("{}\n", 2 * v.0 - v.1));
+        }
+    }
+}
