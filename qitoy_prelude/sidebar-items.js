73db31221ddc0f55e65ced90a53e3e757847619a
qitoy <qitoy@users.noreply.github.com>
2023-11-24 13:54:37 +0000

    deploy: 1d07e7e4acb9a8b228197d840998a5076d7a3f48

--- qitoy_prelude/sidebar-items.js
+++ qitoy_prelude/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"externcrate":["qitoy_utils_bound"],"macro":["chmax","chmin","max","min","mvec"],"trait":["LowerBound","UpperBound"]};
+window.SIDEBAR_ITEMS = {"macro":["chmax","chmin","max","min","mvec"],"trait":["LowerBound","UpperBound"]};
