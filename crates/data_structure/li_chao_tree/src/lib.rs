3268198bd453ee23300f235653a63fdcc6bb6b10
qitoy <itmkan@icloud.com>
2024-05-25 00:02:58 +0900

    とりあえず

--- crates/data_structure/li_chao_tree/src/lib.rs
+++ crates/data_structure/li_chao_tree/src/lib.rs
@@ -61,35 +61,65 @@ impl LiChaoTree {
 
     pub fn add_line(&mut self, a: i64, b: i64) {
         let line = Line { a, b };
-        if let Some(ord) = self.line.cmp(&line, &self.range) {
-            if ord == Greater {
-                self.line = line;
+        let (left_range, right_range) = self.range_divide();
+        match (
+            self.line.cmp(&line, &left_range),
+            self.line.cmp(&line, &right_range),
+        ) {
+            (Some(Equal | Less), Some(Equal | Less)) => ( /* do nothing */ ),
+            (Some(Greater), Some(Greater)) => self.line = line,
+            _ => {
+                self.left_mut().add_line(a, b);
+                self.right_mut().add_line(a, b);
             }
-            return;
-        }
-        let mid = self.mid();
-        let left_range = Range {
-            start: self.range.start,
-            end: mid,
-        };
-        let right_range = Range {
-            start: mid,
-            end: self.range.end,
-        };
-        match (self.line.cmp(&line, &left_range), self.line.cmp(&line, &right_range)) {
-            (None, None) => todo!(),
-            (None, Some(_)) => todo!(),
-            (Some(_), None) => todo!(),
-            (Some(_), Some(_)) => todo!(),
         }
     }
 
     pub fn get_min(&self, x: i64) -> i64 {
-        x
+        let mut min = self.line.get(x);
+        if let Some(left) = self.left() {
+            min = min.min(left.get_min(x));
+        }
+        if let Some(right) = self.right() {
+            min = min.min(right.get_min(x));
+        }
+        min
+    }
+
+    fn left(&self) -> Option<&Self> {
+        self.left.map(|ptr| unsafe { ptr.as_ref() })
+    }
+
+    fn right(&self) -> Option<&Self> {
+        self.right.map(|ptr| unsafe { ptr.as_ref() })
+    }
+
+    fn left_mut(&mut self) -> &mut Self {
+        let (left_range, _) = self.range_divide();
+        unsafe {
+            self.left
+                .get_or_insert_with(|| NonNull::from(Box::leak(Box::new(Self::new(left_range)))))
+                .as_mut()
+        }
+    }
+
+    fn right_mut(&mut self) -> &mut Self {
+        let (_, right_range) = self.range_divide();
+        unsafe {
+            self.right
+                .get_or_insert_with(|| NonNull::from(Box::leak(Box::new(Self::new(right_range)))))
+                .as_mut()
+        }
     }
 
     fn mid(&self) -> i64 {
         let Range { start, end } = self.range;
         (start + end).div_euclid(2)
+    }
+
+    fn range_divide(&self) -> (Range<i64>, Range<i64>) {
+        let Range { start, end } = self.range;
+        let mid = self.mid();
+        (start..mid, mid..end)
     }
 }
