61a63317696b39344de30ead2902ebca8a3b4383
qitoy <itmkan@icloud.com>
2024-05-27 13:02:24 +0900

    impl splay tree

--- crates/data_structure/link_cut_tree/src/lib.rs
+++ crates/data_structure/link_cut_tree/src/lib.rs
@@ -1,36 +1,100 @@
-use std::rc::{Rc, Weak};
+use std::ptr::NonNull;
 
-#[derive(Clone)]
+#[derive(Clone, Copy, Default)]
 struct Node {
-    left: Option<Rc<Node>>,
-    right: Option<Rc<Node>>,
-    parent: Weak<Node>,
-}
-
-impl PartialEq for Node {
-    fn eq(&self, other: &Self) -> bool {
-        self.left == other.left && self.right == other.right
-    }
+    left: Option<NonNull<Self>>,
+    right: Option<NonNull<Self>>,
+    parent: Option<NonNull<Self>>,
 }
 
 impl Node {
-    fn new() -> Rc<Self> {
-        Rc::new(Self {
-            left: None,
-            right: None,
-            parent: Weak::new(),
-        })
+    fn new() -> NonNull<Self> {
+        NonNull::from(Box::leak(Box::default()))
     }
 
-    fn state(self: Rc<Self>) -> i8 {
-        if let Some(par) = self.parent.upgrade() {
-            if par.left == Some(self.clone()) {
-                return -1;
+    fn is_root(this: NonNull<Self>) -> bool {
+        !(Self::is_left_child(this) || Self::is_right_child(this))
+    }
+
+    fn is_left_child(this: NonNull<Self>) -> bool {
+        unsafe {
+            this.as_ref()
+                .parent
+                .is_some_and(|ptr| ptr.as_ref().left == Some(this))
+        }
+    }
+
+    fn is_right_child(this: NonNull<Self>) -> bool {
+        unsafe {
+            this.as_ref()
+                .parent
+                .is_some_and(|ptr| ptr.as_ref().right == Some(this))
+        }
+    }
+
+    fn rotate(mut this: NonNull<Self>) {
+        unsafe {
+            let mut parent = this.as_ref().parent.unwrap();
+            if let Some(mut mid) = if Self::is_left_child(this) {
+                let mid = this.as_ref().right;
+                parent.as_mut().left = mid;
+                this.as_mut().right = Some(parent);
+                mid
+            } else {
+                let mid = this.as_ref().left;
+                parent.as_mut().right = mid;
+                this.as_mut().left = Some(parent);
+                mid
+            } {
+                mid.as_mut().parent = Some(parent);
             }
-            if par.right == Some(self.clone()) {
-                return 1;
+            this.as_mut().parent = parent.as_ref().parent;
+            parent.as_mut().parent = Some(this);
+            if let Some(mut pp) = parent.as_ref().parent {
+                if pp.as_ref().left == Some(parent) {
+                    pp.as_mut().left = Some(this);
+                }
+                if pp.as_ref().right == Some(parent) {
+                    pp.as_mut().right = Some(this);
+                }
             }
         }
-        0
+    }
+
+    fn splay(mut this: NonNull<Self>) {
+        while !Self::is_root(this) {
+            let parent = unsafe { this.as_ref().parent.unwrap() };
+            if Self::is_root(parent) {
+                Self::rotate(this);
+            } else if Self::is_left_child(this) ^ Self::is_left_child(parent) {
+                Self::rotate(this);
+                Self::rotate(this);
+            } else {
+                Self::rotate(parent);
+                Self::rotate(this);
+            }
+        }
+    }
+}
+
+pub struct LinkCutTree {
+    node: Vec<NonNull<Node>>,
+}
+
+impl LinkCutTree {
+    pub fn new(n: usize) -> Self {
+        Self {
+            node: vec![Node::new(); n],
+        }
+    }
+}
+
+impl Drop for LinkCutTree {
+    fn drop(&mut self) {
+        for node in &self.node {
+            unsafe {
+                drop(Box::from_raw(node.as_ptr()));
+            }
+        }
     }
 }
