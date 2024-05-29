4a2556a706b35119b9c784451a3e554f9dbc3983
qitoy <itmkan@icloud.com>
2024-05-29 23:26:34 +0900

    lca verify

---
+++ crates/data_structure/link_cut_tree/examples/lca.rs
@@ -0,0 +1,17 @@
+// verification-helper: PROBLEM https://judge.yosupo.jp/problem/lca
+use proconio::input;
+use itertools::Itertools;
+use qitoy_link_cut_tree::LinkCutTree;
+
+fn main () {
+    input! {
+        n: usize, q: usize,
+        p: [usize; n-1],
+        q: [(usize, usize); q],
+    }
+    let mut lct = LinkCutTree::new(n);
+    for (p, v) in p.into_iter().zip(1..) {
+        lct.link(v, p);
+    }
+    println!("{}", q.into_iter().map(|(u, v)| lct.lca(u, v)).join("\n"));
+}
